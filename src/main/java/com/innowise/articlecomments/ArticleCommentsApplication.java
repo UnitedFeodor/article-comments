9d601c513cf844f86c9abc67412de417f34776b2
Feodor Kachinsky <fedor15zil@gmail.com>
2024-01-24 11:27:06 +0300

    Initial commit

---
+++ src/main/java/com/innowise/articlecomments/ArticleCommentsApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.articlecomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleCommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleCommentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innowise/articlecomments/ArticleCommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.articlecomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleCommentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
