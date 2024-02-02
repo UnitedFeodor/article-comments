26b6ba920d367ef7bb1855a0960badab6445c1ce
Feodor Kachinsky <fedor15zil@gmail.com>
2024-02-02 14:48:19 +0300

    Fix db migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,13 +23,30 @@ repositories {
     mavenCentral()
 }
 
+flyway {
+    url = "jdbc:postgresql://localhost:5432/article_comments"
+    password = "postgres"
+    user = "postgres"
+}
+buildscript {
+    dependencies {
+        classpath("org.postgresql:postgresql:42.2.19")
+        classpath("org.flywaydb:flyway-database-postgresql:10.6.0")
+    }
+}
 dependencies {
+    val mapstructVersion = "1.5.5.Final"
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    annotationProcessor ("org.projectlombok:lombok-mapstruct-binding:0.2.0")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
